35bb2748d28d4ee66131ad65ce7c4f6e748deaf8
GGrop <giuliano.gropponi@gmail.com>
2023-02-24 11:35:08 -0300

    finish clase-5 with bootstrap

--- clase10/tarea-clase5/clase5.js
+++ clase10/tarea-clase5/clase5.js
@@ -1,12 +1,6 @@
-console.log("hola clase 5")
-
-const Calcular=()=>{
-    const botonCalcular = document.querySelector('#botonCalcular')
-    botonCalcular.onclick = function(e){
-        e.preventDefault()
-        const salarioAnual = Number(document.querySelector('#salario-anual').value)
-        document.querySelector('#muestreo-salario').value = salarioAnual*12
-    }
+const botonCalcular = document.querySelector('#botonCalcular')
+botonCalcular.onclick = function(e){
+    e.preventDefault()
+    const salarioAnual = Number(document.querySelector('#salario-anual').value)
+    document.querySelector('#muestreo-salario').value = salarioAnual*12
 }
-
-Calcular()
