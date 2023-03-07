dd449e668ddf5ad8ff744ede3872fe701ea8a4c8
GGrop <giuliano.gropponi@gmail.com>
2023-03-07 08:43:14 -0300

    add 2nd API request
    to save all symbols in symbolList

--- exchangeAPI/src/handleErrors.js
+++ exchangeAPI/src/handleErrors.js
@@ -1,1 +1,15 @@
 let symbolList = [];
+
+var myHeaders = new Headers();
+myHeaders.append("apikey", "K1wPo186HgThBr8LBU4xPtV0QHX2p63S");
+
+var requestOptions = {
+  method: "GET",
+  redirect: "follow",
+  headers: myHeaders,
+};
+fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
+  .then((response) => response.json())
+  .then((result) => (symbolList = Object.keys(result.symbols)))
+  .catch((error) => console.log("error", error));
+
