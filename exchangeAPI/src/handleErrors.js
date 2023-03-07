2c8cb3f6d42bd3f30e710a9f4412db1a1703684d
Giuliano <88691772+GGrop@users.noreply.github.com>
2023-03-07 13:46:46 -0300

    change redundant name symbolList

--- exchangeAPI/src/handleErrors.js
+++ exchangeAPI/src/handleErrors.js
@@ -1,4 +1,4 @@
-let symbolList = [];
+let symbols = [];
 
 var myHeaders = new Headers();
 myHeaders.append("apikey", "K1wPo186HgThBr8LBU4xPtV0QHX2p63S");
@@ -10,11 +10,11 @@ var requestOptions = {
 };
 fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
   .then((response) => response.json())
-  .then((result) => (symbolList = Object.keys(result.symbols)))
+  .then((result) => (symbols = Object.keys(result.symbols)))
   .catch((error) => console.log("error", error));
 
 const validateSymbol = (symbol) => {
-  if (!symbolList.includes(symbol.toUpperCase())) {
+  if (!symbols.includes(symbol.toUpperCase())) {
     if(symbol === ''){
       return "Este campo es obligatorio para la busqueda, pruebe con USD!"
     }
