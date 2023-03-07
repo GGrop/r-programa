b569fd307be7b3655505baa21914d10354a35de9
GGrop <giuliano.gropponi@gmail.com>
2023-03-07 08:22:13 -0300

    add onclick to start the process
    I get the values of inputs, and I get errors if they exist in validate...() function, if they exist, show the alert and the process It´ll never start, but if they dont exist do, continue

--- exchangeAPI/src/index.js
+++ exchangeAPI/src/index.js
@@ -1,1 +1,14 @@
-console.log("world")
+document.querySelector("#send").onclick = function (e) {
+  e.preventDefault();
+  const symbol = document.querySelector("#symbol").value;
+  const date = document.querySelector("#date").value;
+  const symbolErrors = validateSymbol(symbol);
+  const dateErrors = validateDate(date);
+  handleErrors(symbolErrors, dateErrors);
+  if (!symbolErrors && !dateErrors) {
+    loading();
+    removeAlldata();
+    handlerData(symbol, date);
+  }
+};
+const loading = () => {
