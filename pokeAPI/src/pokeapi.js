034e6b453160d1bfc4c9c396b59e566b6045b127
GGrop <giuliano.gropponi@gmail.com>
2023-03-15 22:18:09 -0300

    correct uppercase

--- pokeAPI/src/pokeapi.js
+++ pokeAPI/src/pokeapi.js
@@ -3,7 +3,7 @@ const URL = "https://pokeapi.co/api/v2";
 export async function getPokemons(type = "all") {
   let pokemons;
   if (type === "all") {
-    pokemons = await getAllpokemons();
+    pokemons = await getAllPokemons();
   } else {
     pokemons = await getTypePokemons(type);
   }
